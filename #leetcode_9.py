229cc39bdb171135cd725be9855b999f062d748d
Sangeetha <sangeethapa2002@gmail.com>
2024-02-05 12:30:36 +0530

    Leetcode Problem 9

--- #leetcode_9.py
+++ #leetcode_9.py
@@ -1,9 +1,10 @@
 class Solution:
-    def isPalindrome(self, x: int) -> bool:
+    def isPalindrome(self, x):
         s = x
         rem = 0
         while x > 0:
             rem = (x % 10) + (rem * 10) 
             x = x // 10
         return s == rem
+    
             
