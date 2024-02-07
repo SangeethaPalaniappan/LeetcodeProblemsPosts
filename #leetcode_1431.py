132f752823aa363626f54fb87685d8a8ccd31c83
Sangeetha <sangeethapa2002@gmail.com>
2024-02-07 15:19:04 +0530

    Leetcode Problem 1431

--- #leetcode_1431.py
+++ #leetcode_1431.py
@@ -1,24 +1,11 @@
 class Solution:
     def kidsWithCandies(self, candies, extraCandies):
         maxi = max(candies)
-        
-        start = 0
-        end = len(candies) - 1
-        
 
-        while start <= end:    
-            p = candies[start] + extraCandies
-            q = candies[end] + extraCandies
-            if p >= maxi :
-                candies[start] = 1
-
+        
+        for candy in range(len(candies)):
+            if (candies[candy] + extraCandies) >= maxi:
+                candies[candy] = 1
             else:
-                candies[start] = 0    
-            if q >= maxi :
-                candies[end] = 1  
-                 
-            else:
-                candies[end] = 0
-            start += 1
-            end -= 1    
+                candies[candy] = 0    
         return candies            
