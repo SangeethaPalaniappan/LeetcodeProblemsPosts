3145811da9aba4f46652ef87f37d6997ea7fa54e
Sangeetha <sangeethapa2002@gmail.com>
2024-02-18 10:11:35 +0530

    Majority Element II

---
+++ #leetcode_229.py
@@ -0,0 +1,18 @@
+# 229. Majority Element II
+
+class Solution:
+    def majorityElement(self, nums):
+        arrs = []
+        arr = {}
+        n = len(nums) // 3
+        for k in nums:
+            if k not in arr:
+                arr[k] = 1
+            else:
+                arr[k] += 1
+        for j in arr.keys():
+            get = arr.get(j)
+            if n < get :
+                arrs.append(j)    
+        return arrs        
+
