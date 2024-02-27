416b55189dc1e8c5bc36a7ef963097dd00750cdf
Sangeetha <sangeethapa2002@gmail.com>
2024-02-27 09:47:22 +0530

    Is Subsequence0

---
+++ #leetcode_392.py
@@ -0,0 +1,15 @@
+# 392. Is Subsequence
+
+class Solution:
+    def isSubsequence(self, s, t):
+        length = len(t)
+        init = 0
+        count = 0
+        while init < length and count < len(s):
+            if s[count] == t[init]:
+                count += 1
+
+            init += 1    
+        if len(s) == count:
+            return 1
+        return 0        
