30a9d6df6a3c301bed0ae39a5f08dd149583bca6
Sangeetha <sangeethapa2002@gmail.com>
2024-03-13 22:04:27 +0530

    Monotonic Array

---
+++ #leetcode_896.py
@@ -0,0 +1,18 @@
+# 896. Monotonic Array
+
+class Solution:
+    def isMonotonic(self, nums):
+        init = 0 
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                if init == -1:
+                    return 0
+                else:
+                    init = 1
+            elif nums[i] < nums[i + 1]:
+                if init == 1:
+                    return 0
+                else:
+                    init = -1
+        return 1                            
+
