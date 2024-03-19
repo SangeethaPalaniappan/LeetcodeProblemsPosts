67a07cf7d312fed977bb07fe6c7a0737e9d45cfa
Sangeetha <sangeethapa2002@gmail.com>
2024-03-19 11:32:23 +0530

    Reverse Words in a String III

---
+++ #leetcode_557.py
@@ -0,0 +1,25 @@
+# 557. Reverse Words in a String III
+
+class Solution:
+    def reverseWords(self, s):
+        length = len(s)
+        m = 0
+        n = 0
+        s = list(s)
+        while n < length:
+            k = n    
+            if n  == len(s) - 1 or s[n + 1] == " ":
+                while n > m :    
+                    s[m], s[n] = s[n], s[m]
+                    n -= 1
+                    m += 1
+                
+                n = k + 2
+                m = n          
+            else: 
+                n += 1    
+        arr = ""
+        for i in s:
+            arr += i
+        return arr
+
