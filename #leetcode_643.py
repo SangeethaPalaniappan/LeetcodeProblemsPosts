b61a15d22a602802d1acc9802469266cddeb694a
Sangeetha <sangeethapa2002@gmail.com>
2024-04-20 22:22:27 +0530

    Maximum Average Subarray I

--- #leetcode_643.py
+++ #leetcode_643.py
@@ -1,27 +1,25 @@
 # 643. Maximum Average Subarray I
 
 class Solution:
-    def findMaxAverage(self, nums, k):
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
         n = k
         if k == len(nums):
             return (sum(nums) / k)
         start = 0
         maxi = float('-inf')
-        first_index = 0
-        last_index = k
-
         tot = sum(nums[0 : k]) 
         avg = tot / n
 
         if maxi < avg:
             maxi = avg
 
+        first_index = 0
+        last_index = k
         while last_index < len(nums):
             tot =  (tot - nums[first_index]) + nums[last_index]
             avg = tot / n
             if avg > maxi:
                 maxi = avg
-            k += 1
             first_index += 1
-            last_index = k
+            last_index += 1
         return maxi   
