cfc6415b602af14fd18f5adf8aad07aca47223a8
Sangeetha Palaniappan <122155534+SangeethaPalaniappan@users.noreply.github.com>
2024-07-18 21:01:51 +0530

    Pass the Pillow

---
+++ #leetcode_2582.py
@@ -0,0 +1,11 @@
+# 2582. Pass the Pillow
+
+class Solution:
+    def passThePillow(self, n, time):
+        div = time // (n - 1)
+        rem = time % (n-1)
+        
+        if div % 2 == 0:
+            return rem + 1
+           
+        return n - rem 
