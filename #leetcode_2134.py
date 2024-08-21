cc743d9fb849ebb2af669fc54e8f07e882f929fe
Sangeetha Palaniappan <122155534+SangeethaPalaniappan@users.noreply.github.com>
2024-08-21 22:35:57 +0530

    Minimum Swaps to Group All 1's Together II

---
+++ #leetcode_2134.py
@@ -0,0 +1,24 @@
+# 2134. Minimum Swaps to Group All 1's Together II
+
+class Solution:
+    def minSwaps(self, nums):
+        w_s_count = nums.count(1)
+        i = 0 
+        l_1 = len(nums)
+        if len(nums) != w_s_count:
+            nums.extend(nums)
+        length = len(nums)  
+
+        start = 0
+        end = w_s_count
+        tot = sum(nums[start : end])
+        maxi = tot
+
+        while end < length:
+            tot -= nums[start]
+            tot += nums[end]
+            start += 1
+            end += 1
+            maxi = max(tot, maxi)
+
+        return w_s_count - maxi
