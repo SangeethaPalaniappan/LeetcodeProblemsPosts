cd820e05f7f7858515bd189ea5747278e6a60f9d
Sangeetha Palaniappan <122155534+SangeethaPalaniappan@users.noreply.github.com>
2024-09-28 08:53:08 +0530

    Best Time to Buy and Sell Stock II

--- #leetcode_122.py
+++ #leetcode_122.py
@@ -3,14 +3,8 @@
 class Solution:
     def maxProfit(self, prices):
         profit = 0
-        first = 0 
-        second = 0
-        while second < len(prices): 
-            if prices[first] >= prices[second]:
-                first = second
-            else:
-                diff = prices[second] - prices[first]
-                profit += diff
-                first = second
-            second += 1
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+                
         return profit
